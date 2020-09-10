1e764441ad25754f915d3eac0f019682b1ced0c9
Cristian <cristian@swapps.com>
2020-09-09 20:43:37 -0500

    feat: Total is now a naive range. It is also formatted using suffixes (k and m)

--- src/TotalDisplay.svelte
+++ src/TotalDisplay.svelte
@@ -1,5 +1,19 @@
 <script>
   export let total;
+
+  function kformat(num) {
+    // https://stackoverflow.com/a/9461657/3509554
+    if (Math.abs(num) > 999 && Math.abs(num) < 999999) {
+      return Math.sign(num) * (Math.abs(num) / 1000).toFixed(1) + "k";
+    } else if (Math.abs(num) > 999999) {
+      return Math.sign(num) * (Math.abs(num) / 1000000).toFixed(1) + "m";
+    }
+    return Math.sign(num) * Math.abs(num);
+  }
 </script>
 
-<h2 class="text-xl text-gray-600">Total: {total}</h2>
+{#if total != 0}
+  <h2 class="text-xl text-gray-600">
+    From {kformat(total * 0.75)} To: {kformat(total * 1.25)}
+  </h2>
+{/if}
